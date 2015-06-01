6befa01b4c56669a239837ccce8e1190599d6f9f
vukasin-bozovic <vukasin.bozovic.srb@gmail.com>
2015-06-01 12:12:05 +0200

    added array rules for testing directive -> ng-repeat

--- Data_Binding_and_Directives/common_directives/app.js
+++ Data_Binding_and_Directives/common_directives/app.js
@@ -8,10 +8,19 @@ myApp.controller('mainController', ['$scope', '$filter', '$log', function($scope
 
     $scope.handle = "";
 
+    $scope.characters = 5;
+
     $scope.lowerCaseHandle = function(){
         return $filter("lowercase")($scope.handle);
     };
 
+    $scope.rules = [
+
+        { rulename: "Must be 5 characters" },
+        { rulename: "Must not be used elsewhere" },
+        { rulename: "Must be cool" }
+
+    ];
 
 
 }]);
