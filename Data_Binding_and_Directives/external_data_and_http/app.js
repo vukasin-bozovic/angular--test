62f2b87acccb220d942e161af8b8076e1aa6e70a
vukasin-bozovic <vukasin.bozovic.srb@gmail.com>
2015-06-01 14:52:57 +0200

    tested simple api with $http request

--- Data_Binding_and_Directives/external_data_and_http/app.js
+++ Data_Binding_and_Directives/external_data_and_http/app.js
@@ -4,7 +4,36 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log){
+myApp.controller('mainController', ['$scope', '$http', '$log', function($scope, $http, $log){
+
+    $scope.name = '';
+    $scope.temperature = '';
+    $scope.humidity = '';
+    $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk')
+        .success(function (result){
+            $log.info(result);
+            $scope.name = result.name;
+            $scope.temperature = result.main.temp;
+            $scope.humidity = result.main.humidity;
+        })
+        .error(function(data, status){
+            $log.info(data + status);
+        });
+
+    //This is just example how it should work
+
+    //$scope.newStaff = '';
+    //$scope.addSomething = function(){
+    //    $http.post('http://api.something', { table: $scope.newStaff })
+    //        .success(function(result){
+    //            $log.info(result);
+    //        })
+    //        .error(function(data, status){
+    //            $log.info(data);
+    //        })
+    //}
+
+
 
 
 }]);
