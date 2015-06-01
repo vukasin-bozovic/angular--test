2779a1d2fbf8034c1f4a76c8a67dadd08031d6f7
vukasin-bozovic <vukasin.bozovic.srb@gmail.com>
2015-06-01 18:39:06 +0200

    custom service

---
+++ Custom_services/app.js
@@ -0,0 +1,44 @@
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+   $routeProvider
+
+   .when('/', {
+       templateUrl: 'pages/first.html',
+       controller: 'firstController'
+   })
+
+   .when('/second', {
+       templateUrl: 'pages/second.html',
+       controller: 'secondController'
+   })
+
+   .when('/second/:num', {
+       templateUrl: 'pages/second.html',
+       controller: 'secondController'
+   })
+
+});
+
+
+
+myApp.controller('firstController', ['$scope','$log','$location', function ($scope, $log, $location) {
+
+    $log.info($location.path());
+
+    $scope.name = 'First';
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$location', '$routeParams', function($scope, $log, $location, $routeParams){
+
+    $log.info($location.path());
+    $scope.name = 'Second';
+
+    $scope.num = $routeParams.num || 100;
+
+
+
+}]);
