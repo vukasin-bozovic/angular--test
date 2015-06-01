c9dba095f01508364e884b8da6f33b83990c2f73
vukasin-bozovic <vukasin.bozovic.srb@gmail.com>
2015-06-01 14:15:18 +0200

    init commit

---
+++ Data_Binding_and_Directives/external_data_and_http/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vukasinbozovic on 6/1/15.
+ */
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log){
+
+
+}]);
