4bc49b560a46cb33625c7681f32b823f413f49ea
vukasin-bozovic <vukasin.bozovic.srb@gmail.com>
2015-06-01 22:13:08 +0200

    added first custom service

--- Custom_services/app.js
+++ Custom_services/app.js
@@ -22,22 +22,47 @@ myApp.config(function ($routeProvider) {
 
 });
 
+myApp.service('testService', function(){
+    var self = this;
+
+    self.name = "Vukasin Bozovic";
+
+    self.nameLength = function(){
+        return self.name.length;
+    }
+
+});
 
 
-myApp.controller('firstController', ['$scope','$log','$location', function ($scope, $log, $location) {
+myApp.controller('firstController', ['$scope','$log','$location', 'testService', function ($scope, $log, $location, testService) {
 
     $log.info($location.path());
 
-    $scope.name = 'First';
+    $scope.name = testService.name;
+    $scope.length = testService.nameLength();
+
+    $scope.$watch('name', function(){
+        testService.name = $scope.name;
+    })
+
+
+
 
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', '$location', '$routeParams', function($scope, $log, $location, $routeParams){
+myApp.controller('secondController', ['$scope', '$log', '$location', '$routeParams', 'testService', function($scope, $log, $location, $routeParams, testService){
 
     $log.info($location.path());
     $scope.name = 'Second';
 
     $scope.num = $routeParams.num || 100;
+
+    $scope.name = testService.name;
+    $scope.length = testService.nameLength();
+
+    $scope.$watch('name', function(){
+       testService.name = $scope.name;
+    });
 
 
 
